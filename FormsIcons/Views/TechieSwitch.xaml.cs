8a75242145166b7bbf66f41325d8d8aef4dd814b
jesulink2514 <jesus.angulo@outlook.com>
2020-11-15 00:50:12 -0500

    Finish switch

--- FormsIcons/Views/TechieSwitch.xaml.cs
+++ FormsIcons/Views/TechieSwitch.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +10,69 @@ using Xamarin.Forms.Xaml;
 
 namespace FormsIcons.Views
 {
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class TechieSwitch
+    public partial class TechieSwitch : ITechiesSwitchController
     {
+        public static BindableProperty IsOnProperty = BindableProperty.Create(nameof(IsOn), typeof(bool),
+            typeof(TechieSwitch), false, BindingMode.TwoWay, propertyChanged: OnPropertyChanged);
+
+        public static BindableProperty ThumbBrushProperty = BindableProperty.Create(nameof(ThumbBrush), typeof(Brush),
+            typeof(TechieSwitch), SolidColorBrush.Blue, BindingMode.TwoWay, propertyChanged: OnBrushPropertyChanged);
+
+        public static BindableProperty BorderBrushProperty = BindableProperty.Create(nameof(BorderBrush), typeof(Brush),
+            typeof(TechieSwitch), SolidColorBrush.Blue, BindingMode.TwoWay, propertyChanged: OnBrushPropertyChanged);
+
 
         public TechieSwitch()
         {
             InitializeComponent();
         }
+        public bool IsOn
+        {
+            get => (bool)GetValue(IsOnProperty);
+            set
+            {
+                SetValue(IsOnProperty, value);
+                RaisePropertyChanged(nameof(IsOn));
+                (this as ITechiesSwitchController).SetUITo(value);
+            }
+        }
 
-        private async void ClickGestureRecognizer_OnClicked(object sender, EventArgs e)
+        public Brush ThumbBrush
+        {
+            get => GetValue(ThumbBrushProperty) as Brush;
+            set => SetValue(ThumbBrushProperty, value);
+        }
+
+        public Brush BorderBrush
+        {
+            get => GetValue(BorderBrushProperty) as Brush;
+            set => SetValue(BorderBrushProperty,value);
+        }
+
+        private void ClickGestureRecognizer_OnClicked(object sender, EventArgs e)
+        {
+            (this as ITechiesSwitchController).SetUITo(!IsOn);
+
+            IsOn = !IsOn;
+        }
+
+        private static void OnPropertyChanged(BindableObject bindable, object oldvalue, object newvalue)
+        {
+            (bindable as ITechiesSwitchController).SetUITo((bool)newvalue);
+        }
+        private static void OnBrushPropertyChanged(BindableObject bindable, object oldvalue, object newvalue)
+        {
+            var switchControl = bindable as TechieSwitch;
+            (bindable as ITechiesSwitchController).SetUITo(switchControl.IsOn);
+        }
+        private void RaisePropertyChanged(string propertyName)
+        {
+            this.OnPropertyChanged(propertyName);
+        }
+
+        async void ITechiesSwitchController.SetUITo(bool valueToShow)
         {
             var position = new Rectangle
             {
@@ -28,7 +82,23 @@ namespace FormsIcons.Views
                 Height = 16
             };
 
-            await Thumb.LayoutTo(position, 300, Easing.SinIn);
+            var uncheckedPosition = new Rectangle
+            {
+                Left = 2,
+                Top = 2,
+                Width = 16,
+                Height = 16
+            };
+
+            await Thumb.LayoutTo(valueToShow ? position : uncheckedPosition, 300, Easing.SinIn);
+
+            Thumb.Fill = ThumbBrush;
+            Border.Fill = BorderBrush;
         }
+    }
+
+    internal interface ITechiesSwitchController
+    {
+        void SetUITo(bool valueToShow);
     }
 }
